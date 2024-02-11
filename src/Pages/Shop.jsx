35108c09e154533daf4746f2d5186b86cc9a991d
keneversley <krazorfbx@gmail.com>
2024-02-10 19:44:29 -0500

    Links

---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div className="shop">
+        
+        
+        
+        Shop</div>
+  )
+ 
+}
+
+export default Shop 
