7ab6bed40ebd3648cc7336f78efca591a04a689a
keneversley <krazorfbx@gmail.com>
2024-02-13 18:21:11 -0500

    offers

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
+
 
 export const Shop = () => {
   return (
-    <div className="shop">
-        
-        
-        
-        Shop</div>
+    <div>
+      <Hero/>
+      <Popular/>
+      <Offers/>
+        </div>
   )
  
 }
